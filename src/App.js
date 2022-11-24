e9e5e44474d39164f91f3514b8c1eef35cc0fa4f
Ariga Yeswanth Kumar <102847173+Yeswanthariga@users.noreply.github.com>
2022-11-24 04:19:20 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import DashboardPage from "./pages/dashboard";
+import CoinPage from "./pages/coin";
+import ComparePage from "./pages/compare";
+import WatchListPage from "./pages/watchlist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/watchlist" element={<WatchListPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/dashboard.js
@@ -0,0 +1,92 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "../components/Common/Header";
+import Search from "../components/Dashboard/Search/search";
+import Tabs from "../components/Dashboard/Tabs/tabs";
+import { DASHBOARD_API_URL } from "../constants";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import Loading from "../components/Common/Loading/loading";
+import PaginationComponent from "../components/Dashboard/PaginationComponent/pagination";
+
+function DashboardPage() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageCoins, setPageCoins] = useState([]);
+
+  var filteredCoins = data.filter((item) => {
+    if (
+      item.symbol.toLowerCase().includes(search.toLowerCase()) ||
+      item.name.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return item;
+    }
+  });
+
+  useEffect(() => {
+    axios
+      .get(DASHBOARD_API_URL)
+      .then((response) => {
+        console.log("Response Data >>>", response.data);
+        setData(response.data);
+        setLoading(false);
+        setPageCoins(response.data.slice(0, 10));
+      })
+      .catch((error) => {
+        console.log("Error>>>", error);
+      });
+  }, []);
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  let mybutton = document.getElementById("myBtn");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "flex";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  const handleChange = (event, value) => {
+    setPageNumber(value);
+    setPageCoins(data.slice((value - 1) * 10, (value - 1) * 10 + 10));
+  };
+
+  return (
+    <div>
+      <Header />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Search search={search} setSearch={setSearch} />
+          <Tabs data={search ? filteredCoins : pageCoins} />
+          <div onClick={() => topFunction()} id="myBtn" className="top-btn">
+            <ArrowUpwardIcon sx={{ color: "var(--blue)" }} />
+          </div>
+          {!search && (
+            <PaginationComponent
+              pageNumber={pageNumber}
+              handleChange={handleChange}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default DashboardPage;
